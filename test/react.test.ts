9d24f6e84c5376ded0795139f9dd326c4fbbd64e
Victor Moreno <victor.moreno@cabify.com>
2024-11-22 18:44:35 +0100

    test: add react scope

--- test/react.test.ts
+++ test/react.test.ts
@@ -3,39 +3,41 @@ import { React, render } from "../src/React";
 import { SomeComponent } from "./fixtures/SomeComponent";
 import { JSDOM } from "jsdom";
 
-it("React exists", () => {
-  expect(React).not.to.be.undefined;
-});
+describe('react', () => {
+  it("React exists", () => {
+    expect(React).not.to.be.undefined;
+  });
 
-it("should create elements with tag", () => {
-  const element: any = React.createElement("div");
-  expect(element.tag).equal("div");
-});
+  it("should create elements with tag", () => {
+    const element: any = React.createElement("div");
+    expect(element.tag).equal("div");
+  });
 
-it("should create elements with children", () => {
-  const element: any = React.createElement("div", null, "Hello");
-  expect(element.children).deep.equal(["Hello"]);
-});
+  it("should create elements with children", () => {
+    const element: any = React.createElement("div", null, "Hello");
+    expect(element.children).deep.equal(["Hello"]);
+  });
 
-it("should create elements with props", () => {
-  const element: any = React.createElement("App", { id: "app" });
-  expect(element.props).deep.equal({ id: "app" });
-});
+  it("should create elements with props", () => {
+    const element: any = React.createElement("App", { id: "app" });
+    expect(element.props).deep.equal({ id: "app" });
+  });
 
-it("should allow elements as children", () => {
-  const element: any = React.createElement("div", null, React.createElement("span"));
-  expect(element.children[0].tag).equal("span");
-});
+  it("should allow elements as children", () => {
+    const element: any = React.createElement("div", null, React.createElement("span"));
+    expect(element.children[0].tag).equal("span");
+  });
 
-it("should create components", () => {
-  const element = React.createElement(SomeComponent);
-  expect(element.children).deep.equal(["Hello Component!"]);
-});
+  it("should create components", () => {
+    const element = React.createElement(SomeComponent);
+    expect(element.children).deep.equal(["Hello Component!"]);
+  });
 
-it("should render components", () => {
-  const element = React.createElement(SomeComponent);
-  const window = new JSDOM().window;
-  const container = window.document.createElement("div");
-  render(element, container);
-  expect(container.innerHTML).equal("<div>Hello Component!</div>");
+  it("should render components", () => {
+    const element = React.createElement(SomeComponent);
+    const window = new JSDOM().window;
+    const container = window.document.createElement("div");
+    render(element, container);
+    expect(container.innerHTML).equal("<div>Hello Component!</div>");
+  });
 });
