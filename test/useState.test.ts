6bddaef569f85b003fa8554000fd482edecae988
Victor Moreno <victor.moreno@cabify.com>
2024-11-22 20:02:06 +0100

    feat: updates state

--- test/useState.test.ts
+++ test/useState.test.ts
@@ -5,8 +5,19 @@ describe('useState', () => {
     const [state, _setState] = useState(0);
     expect(state).equal(0);
   });
+
+  it("should update the state", () => {
+    const [_state, setState] = useState(0);
+    setState(1);
+    expect(state).equal(1);
+  });
 });
 
-function useState(initialValue: any) {
-  return [0, 0];
-}
+let state;
+const useState = (initialState: any) => {
+  state = initialState;
+  const setState = (newState: any) => {
+    state = newState;
+  };
+  return [state, setState];
+};
